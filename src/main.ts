e16fb5d08f05a8c4fe612113aac4bbaef51ec8a0
roum02 <roum02@ewhain.net>
2025-03-29 13:17:24 +0900

    Fix origin config

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:8080', 'http://localhost:3000'],
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Blog API')
     .setDescription('블로그 게시글 API 문서')
