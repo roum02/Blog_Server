cbe385b2838ee9cd07d2d25be95f0564d38ca8cc
roum02 <roum02@ewhain.net>
2025-04-19 13:21:55 +0900

    Fix import error

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserModule } from '@user/user.module';
 
 @Module({
   imports: [
     HttpModule,
+    UserModule,
     ConfigModule.forRoot({
       isGlobal: true, // 다른 모듈에서도 .env 사용 가능하게
     }),
